c80c03970f787543073745321696c5f1efe9017c
Dave Pike <david.pike@ouster.io>
2023-11-25 07:12:38 -0500

    Update dockerfiles

--- docker_run.sh
+++ docker_run.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
-# --add-host olive.sfo.ouster.io:10.32.6.14 \
-# --add-host host.docker.internal:host-gateway \
-docker run --rm -ti --gpus all -e DISPLAY=$DISPLAY -e TERM=rxvt-unicode-256color \
-    -v $HOME/container_data:/home/$USER/container_data \
-    -v $HOME/container_data/.ccache:/home/$USER/.ccache \
-    -v $HOME/.ssh:/home/$USER/.ssh \
-    -v $HOME/workspace:/home/$USER/workspace \
-    -v $HOME/data:/home/$USER/data \
+docker run --rm -ti -e DISPLAY=$DISPLAY -e TERM=rxvt-unicode-256color \
+    -v $HOME/container_data:/home/user/container_data \
+    -v $HOME/container_data/.ccache:/home/user/.ccache \
+    -v $HOME/.ssh:/home/user/.ssh \
+    -v $HOME/.conan:/home/user/.conan \
+    -v $HOME/workspace:/home/user/workspace \
     -v /dev:/dev \
-    -e HISTFILE=/home/$USER/container_data/.zsh_history \
+    --add-host olive.sfo.ouster.io:10.32.6.14 \
+    -e HISTFILE=/home/user/container_data/.zsh_history \
     --add-host host.docker.internal:host-gateway \
     --name $1 \
     --hostname $1 \
